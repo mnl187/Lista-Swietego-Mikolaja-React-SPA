8545c711b09a3906fc746e57c54c233491ba81ad
mnl187 <mnl187@gmail.com>
2023-02-09 07:00:06 +0100

    dodanie interface

--- src/components/gifts/GiftsList.tsx
+++ src/components/gifts/GiftsList.tsx
@@ -1,13 +1,15 @@
 import React, {useEffect, useState} from "react";
+import {GiftEntity} from "../../types/gift";
 
 export const GiftsList = () => {
     const [giftsList, setGiftsList] = useState(null);
 
     useEffect(() => {
         (async () => {
-            const res = await fetch('http://localhost:3001/gifts');
+            const res = await fetch('http://localhost:3001/gift');
             const data = await res.json();
             console.log(data);
+            setGiftsList(data.GiftEntity);
 
         })();
     },[]);

9747ca2a0ad78ff227e8bbfc7ed6010b4206e71e
mnl187 <mnl187@gmail.com>
2023-02-10 06:41:29 +0100

    dodanie tabeli giftów

--- src/components/gifts/GiftsTable.tsx
+++ src/components/gifts/GiftsTable.tsx
@@ -5,6 +5,19 @@ interface Props {
     gifts: GiftEntity[];
 }
 
-export const GiftsTable = (props: Props) => {
-
-}
+export const GiftsTable = (props: Props) => (
+    <table>
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Count</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+            props.gifts.map()
+        }
+        </tbody>
+    </table>
+)

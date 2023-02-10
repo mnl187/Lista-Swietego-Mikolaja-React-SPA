fc60bce979286b59462057bd7482d65d76763644
mnl187 <mnl187@gmail.com>
2023-02-10 06:31:43 +0100

    refaktoryzacja GiftsList

--- src/components/gifts/GiftsList.tsx
+++ src/components/gifts/GiftsList.tsx
@@ -2,14 +2,14 @@ import React, {useEffect, useState} from "react";
 import {GiftEntity} from "../../types/gift";
 
 export const GiftsList = () => {
-    const [giftsList, setGiftsList] = useState(null);
+    const [giftsList, setGiftsList] = useState<GiftEntity[] | null>(null);
 
     useEffect(() => {
         (async () => {
             const res = await fetch('http://localhost:3001/gift');
             const data = await res.json();
             console.log(data);
-            setGiftsList(data.GiftEntity);
+            setGiftsList(data.GiftsList);
 
         })();
     },[]);

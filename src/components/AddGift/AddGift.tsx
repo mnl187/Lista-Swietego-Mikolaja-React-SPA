6d61290ffc613b0ca64d6d02a3871282c54c9bc2
mnl187 <mnl187@gmail.com>
2023-02-23 06:51:41 +0100

    dodanie obsługi błędu

--- src/components/AddGift/AddGift.tsx
+++ src/components/AddGift/AddGift.tsx
@@ -1,5 +1,5 @@
 import React, {FormEvent, useState} from 'react';
-import { CreateGiftReq, GiftEntity } from 'types';
+import {CreateGiftReq, GiftEntity} from 'types';
 import {Spinner} from "../common/Spinner/Spinner";
 
 export const AddGift = () => {
@@ -14,7 +14,7 @@ export const AddGift = () => {
     const updateForm = (key: string, value: any) => {
         setForm(form => ({
             ...form,
-           [key]: value,
+            [key]: value,
         }));
     };
 
@@ -23,50 +23,52 @@ export const AddGift = () => {
 
         setLoading(true);
 
-        const res = await fetch(`http://localhost:3001/gift`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(form),
-        });
-        const data: GiftEntity = await res.json();
+        try {
+            const res = await fetch(`http://localhost:3001/gift`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(form),
+            });
+            const data: GiftEntity = await res.json();
 
-        setLoading(false);
-        setResultInfo(`${data.name} added with ID ${data.id}`);
+            setResultInfo(`${data.name} added with ID ${data.id}`);
+        } finally {
+            setLoading(false);
+        }
+
+        if (loading) {
+            return <Spinner/>
+        }
+
+        if (resultInfo !== null) {
+            return <div>
+                <p><strong>{resultInfo}</strong></p>
+                <button onClick={() => setResultInfo(null)}>Add another on</button>
+            </div>
+        }
+
+        return <form onSubmit={sendForm}>
+            <h2>Add Gift</h2>
+            <p>
+
+                <label>
+                    Name <br/>
+                    <input type="text"
+                           value={form.name}
+                           onChange={e => updateForm('name', e.target.value)}/>
+                </label>
+            </p>
+            <p>
+
+                <label>
+                    Count <br/>
+                    <input type="number"
+                           value={form.count}
+                           onChange={e => updateForm('count', Number(e.target.value))}/>
+                </label>
+            </p>
+            <button type="submit">Add</button>
+        </form>
     };
-
-    if (loading) {
-        return <Spinner/>
-    }
-
-if (resultInfo !== null) {
-    return <div>
-        <p><strong>{resultInfo}</strong></p>
-        <button onClick={() => setResultInfo(null)}>Add another on</button>
-    </div>
-}
-
-    return <form onSubmit={sendForm}>
-        <h2>Add Gift</h2>
-        <p>
-
-            <label>
-                Name <br/>
-                <input type="text"
-                       value={form.name}
-                       onChange={e  => updateForm('name', e.target.value)}/>
-            </label>
-        </p>
-        <p>
-
-            <label>
-                Count <br/>
-                <input type="number"
-                       value={form.count}
-                       onChange={e  => updateForm('count', Number(e.target.value))}/>
-            </label>
-        </p>
-        <button type="submit">Add</button>
-    </form>
-};

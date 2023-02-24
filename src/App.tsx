987dcbc548ebdbd4c9f58ba946ec14d66fb71e70
mnl187 <mnl187@gmail.com>
2023-02-24 06:50:34 +0100

    dodanie router w index i App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 import {GiftsView} from "./views/GiftsView";
+import {Route, Routes} from "react-router-dom";
 
 export const App = () => {
     return (
-       <GiftsView/>
+        <Routes>
+            <Route path="/gifts" element={<MainView/>}/>
+            <Route path="/test" element={<MainView/>}/>
+        </Routes>
     );
 }

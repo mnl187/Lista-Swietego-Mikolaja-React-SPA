447e72dfe16c871f84b69b2ed9fedde3fc92ed57
mnl187 <mnl187@gmail.com>
2023-02-24 06:53:17 +0100

    dalsza częśc routera

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import {GiftsView} from "./views/GiftsView";
-import {Route, Routes} from "react-router-dom";
+import {Link, Route, Routes} from "react-router-dom";
 
 export const App = () => {
     return (
+        <>
+            <h1>Santa App</h1>
+            Menu <Link to="/gift">Gift</Link> | <Link to="/test">Gift</Link>
         <Routes>
-            <Route path="/gifts" element={<MainView/>}/>
-            <Route path="/test" element={<MainView/>}/>
+            <Route path="/gift" element={<GiftsView/>}/>
+            <Route path="/test" element={<GiftsView/>}/>
         </Routes>
+        </>
     );
 }

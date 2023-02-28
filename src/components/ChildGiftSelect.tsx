c2cb7c8eeaf4adfabb31bc34a65e1b82735de38e
mnl187 <mnl187@gmail.com>
2023-02-28 06:59:05 +0100

    dodanie haders do wysyłania json

--- src/components/ChildGiftSelect.tsx
+++ src/components/ChildGiftSelect.tsx
@@ -15,6 +15,9 @@ export const ChildGiftSelect = (props: Props) => {
 
         await fetch(`http://localhost:3001/child/gift/${props.childId}`, {
             method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
             body: JSON.stringify({
                 giftId: selected,
             } as SetGiftForChildReq)
